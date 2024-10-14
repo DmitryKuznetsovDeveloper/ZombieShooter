ebea01a4f748dda7bfe92ff33e9df066f78c8130
Dmitry Kuznetsov <pexotaxbox@gmail.com>
2024-10-14 18:45:48 +0300

    F! fix

--- Assets/Scripts/UI/UIExtension/ButtonExtension.cs
+++ Assets/Scripts/UI/UIExtension/ButtonExtension.cs
@@ -10,35 +10,44 @@ namespace UI.UIExtension
         // Добавление анимации при наведении (Highlighted)
         public static void AnimateOnHover(this Button button, Sequence hoverSequence, Ease easeOut, Ease easeIn)
         {
-            EventTrigger trigger = button.gameObject.GetComponent<EventTrigger>() ?? button.gameObject.AddComponent<EventTrigger>();
+            if (hoverSequence == null) throw new ArgumentNullException(nameof(hoverSequence));
 
-            AddEventTrigger(trigger, EventTriggerType.PointerEnter,(eventData) =>
+            EventTrigger trigger = GetOrCreateEventTrigger(button);
+
+            AddOrReplaceEventTrigger(trigger, EventTriggerType.PointerEnter, (eventData) =>
             {
                 if (button.interactable)
-                    hoverSequence.OnComplete(() => hoverSequence.Pause().Complete()).SetEase(easeOut)?.Restart();
+                {
+                    hoverSequence.OnComplete(() => hoverSequence.Pause().Complete()).SetEase(easeOut).Restart();
+                }
             });
 
-            AddEventTrigger(trigger, EventTriggerType.PointerExit,(eventData) =>
+            AddOrReplaceEventTrigger(trigger, EventTriggerType.PointerExit, (eventData) =>
             {
                 if (button.interactable)
-                    hoverSequence.OnComplete(() => hoverSequence.Pause().Complete()).SetEase(easeIn)?.PlayBackwards();
+                {
+                    hoverSequence.OnComplete(() => hoverSequence.Pause().Complete()).SetEase(easeIn).PlayBackwards();
+                }
             });
         }
 
         // Добавление анимации нажатия (Pressed)
         public static void AnimateOnClick(this Button button, Sequence clickSequence, Ease easeOut, Action action)
         {
-            EventTrigger trigger = button.gameObject.GetComponent<EventTrigger>() ?? button.gameObject.AddComponent<EventTrigger>();
-            AddEventTrigger(trigger, EventTriggerType.PointerClick,(eventData) =>
+            if (clickSequence == null) throw new ArgumentNullException(nameof(clickSequence));
+            if (action == null) throw new ArgumentNullException(nameof(action));
+
+            EventTrigger trigger = GetOrCreateEventTrigger(button);
+
+            AddOrReplaceEventTrigger(trigger, EventTriggerType.PointerClick, (eventData) =>
             {
                 if (button.interactable)
                 {
                     clickSequence.OnComplete(() =>
-                        {
-                            clickSequence.Pause().Complete();
-                            action?.Invoke();
-                        })
-                        .SetEase(easeOut)?.Restart();
+                    {
+                        clickSequence.Pause().Complete();
+                        action.Invoke();
+                    }).SetEase(easeOut).Restart();
                 }
             });
         }
@@ -46,53 +55,86 @@ namespace UI.UIExtension
         // Добавление анимации при выборе (Selected)
         public static void AnimateOnSelected(this Button button, Sequence selectedSequence)
         {
-            EventTrigger trigger = button.gameObject.GetComponent<EventTrigger>() ?? button.gameObject.AddComponent<EventTrigger>();
-            AddEventTrigger(trigger, EventTriggerType.Select,(eventData) =>
+            if (selectedSequence == null) throw new ArgumentNullException(nameof(selectedSequence));
+
+            EventTrigger trigger = GetOrCreateEventTrigger(button);
+
+            AddOrReplaceEventTrigger(trigger, EventTriggerType.Select, (eventData) =>
             {
                 if (button.interactable)
-                    selectedSequence.OnComplete(()=> selectedSequence.Pause().Complete())?.Restart();
+                {
+                    selectedSequence.OnComplete(() => selectedSequence.Pause().Complete()).Restart();
+                }
             });
 
-            AddEventTrigger(trigger, EventTriggerType.Deselect, (eventData) =>
+            AddOrReplaceEventTrigger(trigger, EventTriggerType.Deselect, (eventData) =>
             {
                 if (button.interactable)
-                    selectedSequence.OnComplete(()=> selectedSequence.Pause().Complete())?.PlayBackwards();
+                {
+                    selectedSequence.OnComplete(() => selectedSequence.Pause().Complete()).PlayBackwards();
+                }
             });
         }
 
         // Добавление анимации выключения кнопки (Disabled)
         public static void AnimateOnDisabled(this Button button, Sequence disabledSequence)
         {
+            if (disabledSequence == null) throw new ArgumentNullException(nameof(disabledSequence));
+
             if (!button.interactable)
-                disabledSequence.OnComplete((() => disabledSequence.Pause().Complete()))?.Restart();
+            {
+                disabledSequence.OnComplete(() => disabledSequence.Pause().Complete()).Restart();
+            }
         }
-        
+
         // Метод для отписки от событий
         public static void UnsubscribeAll(this Button button)
         {
             EventTrigger trigger = button.gameObject.GetComponent<EventTrigger>();
 
-            // Если EventTrigger существует, очищаем все записи
-            if (trigger is not null)
-                trigger.triggers.Clear(); // Очищаем все триггеры
+            if (trigger != null)
+            {
+                trigger.triggers.Clear();
+            }
 
-            // Удаляем все подписки на события кнопки
             button.onClick.RemoveAllListeners();
         }
-        public static void UnsubscribeSequence(this Button button,Sequence hoverSequence = null, Sequence clickSequence = null, Sequence selectedSequence = null, Sequence disabledSequence = null)
-        { 
+
+        // Метод для отписки от конкретных последовательностей
+        public static void UnsubscribeSequence(this Button button, Sequence hoverSequence = null, Sequence clickSequence = null, Sequence selectedSequence = null, Sequence disabledSequence = null)
+        {
             hoverSequence?.Kill();
             clickSequence?.Kill();
             selectedSequence?.Kill();
             disabledSequence?.Kill();
         }
 
-        // Добавления событий в EventTrigger
-        private static void AddEventTrigger(EventTrigger trigger, EventTriggerType eventType, System.Action<BaseEventData> action)
+        // Получение или создание EventTrigger
+        private static EventTrigger GetOrCreateEventTrigger(Button button)
         {
-            EventTrigger.Entry entry = new EventTrigger.Entry { eventID = eventType };
-            entry.callback.AddListener((eventData) => action(eventData));
-            trigger.triggers.Add(entry);
+            EventTrigger trigger = button.gameObject.GetComponent<EventTrigger>();
+            if (trigger == null)
+            {
+                trigger = button.gameObject.AddComponent<EventTrigger>();
+            }
+            return trigger;
+        }
+
+        // Добавление или замена событий в EventTrigger
+        private static void AddOrReplaceEventTrigger(EventTrigger trigger, EventTriggerType eventType, Action<BaseEventData> action)
+        {
+            EventTrigger.Entry existingEntry = trigger.triggers.Find(entry => entry.eventID == eventType);
+            if (existingEntry != null)
+            {
+                existingEntry.callback.RemoveAllListeners();
+                existingEntry.callback.AddListener((eventData) => action(eventData));
+            }
+            else
+            {
+                EventTrigger.Entry newEntry = new EventTrigger.Entry { eventID = eventType };
+                newEntry.callback.AddListener((eventData) => action(eventData));
+                trigger.triggers.Add(newEntry);
+            }
         }
     }
 }
